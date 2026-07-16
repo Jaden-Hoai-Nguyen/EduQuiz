a96cd769377682f5619beef776881d724cf6cc7d
Jaden <bao.nh@iigvietnam.edu.vn>
2026-07-16 15:34:33 +0700

    save_sheet

--- js/googleSheet.js
+++ js/googleSheet.js
@@ -14,7 +14,7 @@
 
 // 🔗 Dán URL Google Apps Script Web App của bạn vào đây
 // Ví dụ: 'https://script.google.com/macros/s/AKfy.../exec'
-const APPS_SCRIPT_URL = 'https://docs.google.com/spreadsheets/d/1KPXrvgrEDhO_bMPxOBvIdgeo1PPWx6bA9F89Itos2cQ/edit?usp=sharing';
+const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxjhgkU7lRQ95wO6CLRBEpb_UXICeWp8ObON7_zHrZsoEwZc9YIRv9PsT31c9psnGp_/exec';
 
 // ⏱️ Timeout: Nếu request quá 10 giây → báo lỗi (đơn vị: ms)
 const REQUEST_TIMEOUT_MS = 10000;
